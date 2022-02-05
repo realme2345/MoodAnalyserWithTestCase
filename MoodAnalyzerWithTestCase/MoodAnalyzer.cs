738d0033ee12b292a1b7e7364975c5236fcfc71b
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-05 11:18:30 +0530

    [Raja]Add Moood of user implemented Mood analyzer

---
+++ MoodAnalyzerWithTestCase/MoodAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerWithTestCase
+{/// <summary>
+/// Mood Analyzer
+/// </summary>
+    class MoodAnalyzer
+    {
+        public string Message;
+        public void DisplayMoodAnalyzer(string message) //Creating a constructer 
+        {
+            Message = message;
+            if (Message.ToLower().Contains("happy"))
+            {
+               Console.WriteLine("Happy");
+            }
+            else
+            {
+                Console.WriteLine("Sad");
+            }
+        } 
+    }
+}
--- MoodAnalyzerWithTestCase/Program.cs
+++ MoodAnalyzerWithTestCase/Program.cs
@@ -11,6 +11,8 @@ namespace MoodAnalyzerWithTestCase
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Moodanalyzer with test case");
+            MoodAnalyzer moodAnalyzer=new MoodAnalyzer(); //Creating object for a class
+            moodAnalyzer.DisplayMoodAnalyzer("I am in Happy mood");
         }
     }
 }
